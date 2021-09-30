6a1d120344561416d352985eadfc96411386843b
dani <dani.zaghian@gmail.com>
2021-09-30 12:05:25 -0700

    adds new hardcoded recipe

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="home">
+    <h1>New Recipe</h1>
+    <button v-on:click="createRecipe()">Create</button>
     <h1>All Recipes</h1>
     <div v-for="recipe in recipes" v-bind:key="recipe.id">
       <h2>Title: {{ recipe.title }}</h2>
@@ -34,6 +36,26 @@ export default {
         console.log(response.data);
         this.recipes = response.data;
       });
+    },
+    createRecipe: function () {
+      var params = {
+        title: "Example title",
+        ingredients: "Example Ingredients",
+        directions: "Example Directions",
+        image_url: "Example Image",
+        prep_time: 25
+      };
+      axios
+        .post("http://localhost:3000/recipes", params)
+        // happy status code/path
+        .then((response) => {
+          console.log(response.data);
+          this.recipes.push(response.data);
+        })
+        // sad status code/path
+        .catch((error) => {
+          console.log(error.response.data.errors);
+        });
     }
   }
 };
