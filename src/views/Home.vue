8e1e411ba41e67deb99c749f3045afc017d139d3
dani <dani.zaghian@gmail.com>
2021-09-30 10:27:27 -0700

    adds recipes array and v-for

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,25 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>All Recipes</h1>
+    <div v-for="recipe in recipes" v-bind:key="recipe.id">
+      <h2>Title: {{ recipe.title }}</h2>
+      <p>Prep time: {{ recipe.prep_time }}</p>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
-  name: "Home",
-  components: {
-    HelloWorld,
+  data: function () {
+    return {
+      recipes: [
+        { id: 1, title: "Raw Eggs", prep_time: 20 },
+        { id: 2, title: "Empty Sandwich", prep_time: 5 },
+        { id: 3, title: "Ice Cubes", prep_time: 120 }
+      ]
+    };
   },
+  created: function () {},
+  methods: {}
 };
 </script>
