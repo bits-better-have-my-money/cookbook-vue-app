cc6c93b599edb484a931ac8c9f6ca616e743dd08
dani <dani.zaghian@gmail.com>
2021-10-12 09:46:30 -0700

    adds sorting

--- src/views/RecipesIndex.vue
+++ src/views/RecipesIndex.vue
@@ -2,12 +2,18 @@
   <div class="recipes-index">
     <h1>All Recipes</h1>
     <div>Search: <input type="text" v-model="titleFilter" /></div>
+    <button v-on:click="sortAttribute = 'title'">Sort by title</button>
+    <button v-on:click="sortAttribute = 'prep_time'">Sort by prep time</button>
     <div
-      v-for="recipe in filterBy(recipes, titleFilter, 'title')"
+      v-for="recipe in orderBy(
+        filterBy(recipes, titleFilter, 'title'),
+        sortAttribute
+      )"
       v-bind:key="recipe.id"
     >
       <h4>{{ recipe.title }}</h4>
       <img :src="recipe.image_url" alt="" />
+      <p>Prep time: {{ recipe.prep_time }}</p>
       <p>Created {{ relativeDate(recipe.created_at) }}</p>
       <p v-if="$parent.getUserId() == recipe.user.id">Your recipe</p>
       <router-link :to="`/recipes/${recipe.id}`">See Details</router-link>
@@ -27,7 +33,8 @@ export default {
   data: function () {
     return {
       recipes: [],
-      titleFilter: ""
+      titleFilter: "",
+      sortAttribute: "title"
     };
   },
   created: function () {
