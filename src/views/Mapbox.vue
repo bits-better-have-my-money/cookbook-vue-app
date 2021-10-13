08f9a758fc80b049f77bd2a7d7451701312a2477
dani <dani.zaghian@gmail.com>
2021-10-13 11:25:28 -0700

    adds multiple map markers and popups to the map

--- src/views/Mapbox.vue
+++ src/views/Mapbox.vue
@@ -15,7 +15,21 @@
 /* global mapboxgl */
 export default {
   data: function () {
-    return {};
+    return {
+      places: [
+        {
+          lat: 37.79207188214143,
+          lng: -122.42103996911959,
+          description: "Bob's Donuts in SF - home of delicious giant donuts"
+        },
+        {
+          lat: 37.779049476903005,
+          lng: -122.42327655933346,
+          description: "Johnny's Donuts - best fancy donuts in the bay"
+        },
+        { lat: -25.363, lng: 131.044, description: "A place in Australia" }
+      ]
+    };
   },
   mounted: function () {
     mapboxgl.accessToken = process.env.VUE_APP_MAPBOX_ACCESS_TOKEN;
@@ -26,17 +40,16 @@ export default {
       zoom: 12 // starting zoom
     });
 
-    // create the popup
-    const popup = new mapboxgl.Popup({ offset: 25 }).setText(
-      "This is the heart of San Francisco"
-    );
-
-    // Create a default Marker and add it to the map.
-    const marker1 = new mapboxgl.Marker()
-      .setLngLat([-122.43, 37.77])
-      .setPopup(popup)
-      .addTo(map);
-    console.log(marker1);
+    this.places.forEach((place) => {
+      const popup = new mapboxgl.Popup({ offset: 25 }).setText(
+        place.description
+      );
+      const marker = new mapboxgl.Marker()
+        .setLngLat([place.lng, place.lat])
+        .setPopup(popup)
+        .addTo(map);
+      console.log(marker);
+    });
   },
   methods: {}
 };
